d064cd5bcb2511e76e090844c449199ca8a7a1cb
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-10-20 11:13:01 -0400

    core: document mod command

--- BlasII.CheatConsole/ModCommand.cs
+++ BlasII.CheatConsole/ModCommand.cs
@@ -2,17 +2,29 @@
 
 namespace BlasII.CheatConsole;
 
-public abstract class ModCommand
+/// <summary>
+/// A command that can be executed in the cheat console
+/// </summary>
+public abstract class ModCommand(string name)
 {
-    private readonly string _name;
-    public string Name => _name;
+    /// <summary>
+    /// The name used to execute the command
+    /// </summary>
+    public string Name { get; } = name;
 
-    public ModCommand(string name) => _name = name;
-
+    /// <summary>
+    /// Performs any functions when the command is executed
+    /// </summary>
     public abstract void Execute(string[] args);
 
+    /// <summary>
+    /// Performs any functions every game frame
+    /// </summary>
     public virtual void Update() { }
 
+    /// <summary>
+    /// Ensures there is a certain amount of parameters
+    /// </summary>
     protected bool ValidateParameterCount(string[] paramaters, int num)
     {
         bool isValid = paramaters.Length == num;
@@ -23,6 +35,9 @@ public abstract class ModCommand
         return isValid;
     }
 
+    /// <summary>
+    /// Ensures the parameter is a valid integer
+    /// </summary>
     protected bool ValidateIntParamater(string parameter, out int result)
     {
         bool isValid = int.TryParse(parameter, out result);
@@ -33,7 +48,13 @@ public abstract class ModCommand
         return isValid;
     }
 
+    /// <summary>
+    /// Logs an info message to the console
+    /// </summary>
     protected void Write(string message) => ModLog.Info("[CONSOLE] " + message);
 
+    /// <summary>
+    /// Logs an error message to the console
+    /// </summary>
     protected void WriteFailure(string message) => ModLog.Error("[CONSOLE] " + message);
 }

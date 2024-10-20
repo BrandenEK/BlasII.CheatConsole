12d5da5fc299696e7662194876300a83f37b3765
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-10-20 11:16:18 -0400

    core: add command register

---
+++ BlasII.CheatConsole/CommandRegister.cs
@@ -0,0 +1,27 @@
+﻿using BlasII.ModdingAPI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlasII.CheatConsole;
+
+/// <summary>
+/// Registers a new console command
+/// </summary>
+public static class CommandRegister
+{
+    private static readonly List<ModCommand> _commands = [];
+    internal static IEnumerable<ModCommand> Commands => _commands;
+
+    /// <summary> Registers a new console command </summary>
+    public static void RegisterCommand(this ModServiceProvider provider, ModCommand command)
+    {
+        if (provider == null)
+            return;
+
+        if (string.IsNullOrEmpty(command.Name) || _commands.Any(cmd => cmd.Name == command.Name))
+            return;
+
+        _commands.Add(command);
+        ModLog.Info($"Registered command: {command.Name}");
+    }
+}

22234ce4c30c2eeb4a73848cab7880b129e8573e
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-01-31 14:24:03 -0500

    console: improve history traversal

--- BlasII.CheatConsole/CommandHistory.cs
+++ BlasII.CheatConsole/CommandHistory.cs
@@ -31,10 +31,8 @@ public class CommandHistory
         if (_pastCommands.Count == 0)
             return string.Empty;
 
-        if (_currIdx >= _pastCommands.Count - 1)
-            return _pastCommands[_pastCommands.Count - 1];
-
-        return _pastCommands[++_currIdx];
+        _currIdx = _currIdx >= _pastCommands.Count - 1 ? _pastCommands.Count - 1 : _currIdx + 1;
+        return _pastCommands[_currIdx];
     }
 
     /// <summary>
@@ -45,10 +43,8 @@ public class CommandHistory
         if (_pastCommands.Count == 0)
             return string.Empty;
 
-        if (_currIdx <= 0)
-            return _pastCommands[0];
-
-        return _pastCommands[--_currIdx];
+        _currIdx = _currIdx <= 0 ? 0 : _currIdx - 1;
+        return _pastCommands[_currIdx];
     }
 
     private const int MAX_COMMANDS = 10;

4cfeacf0f97b3c9c8a20918856e0d4745e1f6973
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-01-31 21:31:36 -0500

    commands: add conditional command

---
+++ BlasII.CheatConsole/Conditionals/ConditionalCommand.cs
@@ -0,0 +1,41 @@
+﻿using BlasII.ModdingAPI;
+using System.Collections.Generic;
+
+namespace BlasII.CheatConsole.Conditionals;
+
+/// <summary>
+/// A command with easier argument conditions
+/// </summary>
+public abstract class ConditionalCommand : ModCommand
+{
+    private readonly IEnumerable<ConditionalTarget> _targets;
+
+    /// <inheritdoc/>
+    protected ConditionalCommand(string name) : base(name)
+    {
+        _targets = InitializeTargets();
+    }
+
+    /// <summary>
+    /// Executes the first target with a satisfied condition
+    /// </summary>
+    public sealed override void Execute(string[] args)
+    {
+        foreach (var target in _targets)
+        {
+            if (!target.Condition(args))
+                continue;
+
+            target.Execution(args);
+            return;
+        }
+
+        // Make better
+        WriteFailure("Error");
+    }
+
+    /// <summary>
+    /// Stores all conditional targets in the constructor
+    /// </summary>
+    protected abstract IEnumerable<ConditionalTarget> InitializeTargets();
+}

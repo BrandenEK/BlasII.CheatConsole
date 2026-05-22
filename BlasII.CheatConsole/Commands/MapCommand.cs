db43b0520e44fee5a399cc59ea4e39ceb120902a
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-05-22 09:59:34 -0400

    commands: add map command (#30)
    * commands: add map command
    
    * commands: implement map reveal

---
+++ BlasII.CheatConsole/Commands/MapCommand.cs
@@ -0,0 +1,27 @@
+﻿using Il2CppSystem.Linq;
+using Il2CppTGK.Game;
+using System.Linq;
+
+namespace BlasII.CheatConsole.Commands;
+
+internal class MapCommand : ModCommand
+{
+    public MapCommand() : base("map") { }
+
+    public override void Execute(string[] args)
+    {
+        if (!ValidateParameterCount(args, 1))
+            return;
+
+        if (args[0] != "reveal")
+        {
+            WriteFailure("Unknown subcommand: " + args[0]);
+            return;
+        }
+
+        Write("Revealing entire map");
+        
+        foreach (var cell in CoreCache.Map.GetAllCells().ToArray())
+            CoreCache.Map.RevealCellInPositionWholeMap(cell.key.GetVector2());
+    }
+}

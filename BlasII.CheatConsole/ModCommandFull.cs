db0d195aeea880ef0e042639089b095bba970d4c
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-08-25 11:44:47 -0400

    core: add mod command full

---
+++ BlasII.CheatConsole/ModCommandFull.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlasII.CheatConsole;
+
+internal class ModCommandFull : ModCommand
+{
+    public ModCommandFull(string name) : base(name)
+    {
+        // Use reflection to cache the subcommands
+    }
+
+    public override void Execute(string[] args)
+    {
+        // help or no parameters will list the valid descriptions
+
+        throw new NotImplementedException();
+    }
+}

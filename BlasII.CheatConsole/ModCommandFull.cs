3237c462dc2ef49a397aa9d58c9e4606a575514f
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-08-25 12:43:27 -0400

    core: add subcommand attribute for commands (#45)
    * core: add sub command attribute
    
    * core: add mod command full
    
    * core: add new test wep command
    
    * core: reflection to get method infos
    
    * core: display subcommand help
    
    * core: parse parameters and execute method
    
    * core: cleanup

---
+++ BlasII.CheatConsole/ModCommandFull.cs
@@ -0,0 +1,94 @@
+﻿using BlasII.CheatConsole.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BlasII.CheatConsole;
+
+internal class ModCommandFull : ModCommand
+{
+    private readonly Dictionary<string, MethodInfo> _subcommands;
+
+    public override bool NeedsParameters => false;
+
+    public ModCommandFull(string name) : base(name)
+    {
+        _subcommands = GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+            .Where(x => x.IsDefined(typeof(SubCommandAttribute), false))
+            .ToDictionary(x => x.Name.ToLower(), x => x);
+    }
+
+    public override void Execute(string[] args)
+    {
+        // Typing no parameters or typing help will list the possible subcommands
+        if (args.Length < 1 || args[0].ToLower() == "help")
+        {
+            DisplayHelp();
+            return;
+        }
+
+        // Ensure there is a valid subcommand for the name
+        if (!_subcommands.TryGetValue(args[0], out MethodInfo subcommand))
+        {
+            WriteFailure($"'{args[0]}' is not a valid subcommand.  Enter '{Name} help' to see a list of valid subcommands.");
+            return;
+        }
+
+        ParameterInfo[] parameters = subcommand.GetParameters();
+
+        // Ensure the number of parameters matches the subcommand
+        if (args.Length - 1 != parameters.Length)
+        {
+            WriteFailure($"The subcommand '{Name} {args[0]}' expects {parameters.Length} parameters.  You passed {args.Length - 1}.");
+            return;
+        }
+
+        object[] arguments = new object[parameters.Length];
+
+        // Create and parse the list of parameters
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            string input = args[i + 1];
+            Type type = parameters[i].ParameterType;
+
+            try
+            {
+                arguments[i] = ParseParameter(input, type);
+            }
+            catch
+            {
+                WriteFailure($"Failed to parse '{input}' to a {type.Name}.");
+                return;
+            }
+        }
+
+        subcommand.Invoke(this, arguments);
+    }
+
+    private object ParseParameter(string input, Type type)
+    {
+        return Type.GetTypeCode(type) switch
+        {
+            TypeCode.Boolean => Convert.ToBoolean(input),
+            TypeCode.Int32 => Convert.ToInt32(input),
+            TypeCode.Single => Convert.ToSingle(input),
+            TypeCode.String => input,
+            _ => throw new NotSupportedException($"Parameter type '{type.Name}' is not supported"),
+        };
+    }
+
+    private void DisplayHelp()
+    {
+        Write($"Possible subcommands for {Name}:");
+        foreach (var method in _subcommands.Values)
+        {
+            string command = $"{Name} {method.Name.ToLower()}";
+            foreach (var param in method.GetParameters())
+                command += $" {{{param.Name.ToUpper()}}}";
+
+            Write(command);
+        }
+        Write(string.Empty);
+    }
+}

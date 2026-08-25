89a50af9962a4901651abf3e7b3e3d01800eac60
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-08-25 11:51:08 -0400

    core: add new test wep command

--- BlasII.CheatConsole/CheatConsole.cs
+++ BlasII.CheatConsole/CheatConsole.cs
@@ -1,4 +1,5 @@
 ﻿using BlasII.CheatConsole.Commands;
+using BlasII.CheatConsole.FullCommands;
 using BlasII.Framework.UI;
 using BlasII.ModdingAPI;
 using BlasII.ModdingAPI.Assets;
@@ -248,5 +249,7 @@ public class CheatConsole : BlasIIMod
 
         provider.RegisterCommand(new GodmodeCommand());
         provider.RegisterCommand(new LoadCommand());
+
+        provider.RegisterCommand(new NewWeaponCommand());
     }
 }
---
+++ BlasII.CheatConsole/FullCommands/NewWeaponCommand.cs
@@ -0,0 +1,35 @@
+﻿using BlasII.CheatConsole.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlasII.CheatConsole.FullCommands;
+
+internal class NewWeaponCommand : ModCommandFull
+{
+    public NewWeaponCommand() : base("wep") { }
+
+    [SubCommand("list", 0)]
+    private void List()
+    {
+        Write("list all weapons");
+    }
+
+    [SubCommand("lock", 1)]
+    private void Lock(string id)
+    {
+        Write("lock wep id");
+    }
+
+    private void Unlock(string id)
+    {
+        Write("unlock wep id");
+    }
+
+    private void Upgrade(string id)
+    {
+        Write("upgrade wep id");
+    }
+}

9cc0adca0c6332b5179d1ef6e0649833414ff10f
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-08-25 11:41:17 -0400

    core: add sub command attribute

---
+++ BlasII.CheatConsole/Attributes/SubCommandAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BlasII.CheatConsole.Attributes;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+internal class SubCommandAttribute : Attribute
+{
+    public string Name { get; }
+
+    public int NumParameters { get; }
+
+    public SubCommandAttribute(string name, int numParameters)
+    {
+        Name = name;
+        NumParameters = numParameters;
+    }
+}

9a8c1b4f5b6d7645c1321c262ff043290abfd481
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-09-06 10:01:48 -0400

    commands: implement skin command

--- BlasII.CheatConsole/Commands/Complex/SkinCommand.cs
+++ BlasII.CheatConsole/Commands/Complex/SkinCommand.cs
@@ -1,4 +1,8 @@
 ﻿using BlasII.CheatConsole.Attributes;
+using Il2CppTGK.Game;
+using Il2CppTGK.Game.Managers;
+using System.Linq;
+using UnityEngine;
 
 namespace BlasII.CheatConsole.Commands.Complex;
 
@@ -9,24 +13,44 @@ internal class SkinCommand : ModComplexCommand
     [SubCommand]
     private void List()
     {
+        var palettes = Resources.FindObjectsOfTypeAll<PaletteID>().OrderBy(x => x.name);
 
+        Write("Available skins:");
+        foreach (var palette in palettes)
+        {
+            Write(palette.name);
+        }
     }
 
     [SubCommand]
     private void Unlock(string id)
     {
+        var palettes = Resources.FindObjectsOfTypeAll<PaletteID>().OrderBy(x => x.name);
+        var skin = palettes.FirstOrDefault(x => x.name == id.ToUpper());
 
-    }
+        if (skin == null)
+        {
+            WriteFailure($"The skin {id} does not exist.");
+            return;
+        }
 
-    [SubCommand]
-    private void Lock(string id)
-    {
-
+        Write($"Unlocking skin {skin.name}");
+        CoreCache.PlayerRecolorManager.UnlockSkin(skin);
     }
 
     [SubCommand]
     private void Set(string id)
     {
+        var palettes = Resources.FindObjectsOfTypeAll<PaletteID>().OrderBy(x => x.name);
+        var skin = palettes.FirstOrDefault(x => x.name == id.ToUpper());
 
+        if (skin == null)
+        {
+            WriteFailure($"The skin {id} does not exist.");
+            return;
+        }
+
+        Write($"Setting current skin to {skin.name}");
+        CoreCache.PlayerRecolorManager.SetPalette(skin);
     }
 }

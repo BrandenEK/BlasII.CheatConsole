a265a2a8b70c635a4267c87db2477852cd2da5c8
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-09-06 09:50:33 -0400

    commands: create new skin command

---
+++ BlasII.CheatConsole/Commands/Complex/SkinCommand.cs
@@ -0,0 +1,32 @@
+﻿using BlasII.CheatConsole.Attributes;
+
+namespace BlasII.CheatConsole.Commands.Complex;
+
+internal class SkinCommand : ModComplexCommand
+{
+    public SkinCommand() : base("skin") { }
+
+    [SubCommand]
+    private void List()
+    {
+
+    }
+
+    [SubCommand]
+    private void Unlock(string id)
+    {
+
+    }
+
+    [SubCommand]
+    private void Lock(string id)
+    {
+
+    }
+
+    [SubCommand]
+    private void Set(string id)
+    {
+
+    }
+}

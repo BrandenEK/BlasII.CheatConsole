a22e12c2d42371381eecba4d4d8ad6d9022ae44a
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-09-06 08:40:24 -0400

    commands: add simple command

---
+++ BlasII.CheatConsole/Commands/ModSimpleCommand.cs
@@ -0,0 +1,43 @@
+﻿using BlasII.CheatConsole.Attributes;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace BlasII.CheatConsole.Commands;
+
+internal class ModSimpleCommand : ModCommand
+{
+    private readonly MethodInfo _command;
+
+    public override bool NeedsParameters => false;
+
+    public ModSimpleCommand(string name) : base(name)
+    {
+        _command = GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+            .FirstOrDefault(x => x.IsDefined(typeof(MainCommandAttribute), false)) ?? throw new InvalidOperationException($"The command {Name} is missing a {nameof(MainCommandAttribute)} method");
+    }
+
+    public override void Execute(string[] args)
+    {
+        // Typing no parameters or typing help will list the possible subcommands
+        if (args.Length < 1 || args[0].ToLower() == "help")
+        {
+            DisplayHelp();
+            return;
+        }
+
+        Write("Running command");
+    }
+
+    private void DisplayHelp()
+    {
+        Write($"Syntax for {Name}:");
+
+        string command = $"{Name}";
+        foreach (var param in _command.GetParameters())
+            command += $" {{{param.Name.ToUpper()}}}";
+
+        Write(command);
+        Write(string.Empty);
+    }
+}
---
+++ BlasII.CheatConsole/Commands/Simple/TestCommand.cs
@@ -0,0 +1,19 @@
+﻿using BlasII.CheatConsole.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlasII.CheatConsole.Commands.Simple;
+
+internal class TestCommand : ModSimpleCommand
+{
+    public TestCommand() : base("test") { }
+
+    [MainCommand]
+    private void Execute(string status, int numTimes)
+    {
+        Write($"Performing {status} {numTimes} times");
+    }
+}
